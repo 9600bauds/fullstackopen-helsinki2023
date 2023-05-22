b96f640b1c7e40a0a5bc5bc5232dd10f1066f4c2
9600bauds <blasaladio3@gmail.com>
2023-05-22 15:54:38 -0300

    Exercise 1.6.

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const Label = ({ text }) => {
+    return <span>{text}</span>
+}
+
+const Button = ({ clickAction, text }) => {
+    return (
+        <button onClick={clickAction}>
+            {text}
+        </button>
+        )
+}
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const increaseGood = (amt) => {
+        const updatedGood = good + amt
+        setGood(updatedGood)
+    }
+    const increaseNeutral = (amt) => {
+        const updatedNeutral = neutral + amt
+        setNeutral(updatedNeutral)
+    }
+    const increaseBad = (amt) => {
+        const updatedBad = bad + amt
+        setBad(updatedBad)
+    }
+
+    return (
+        <div>
+            <h1>Give Feedback</h1>
+
+            <Button clickAction={() => increaseGood(1)} text="Good" />
+            <Button clickAction={() => increaseNeutral(1)} text="Neutral" />
+            <Button clickAction={() => increaseBad(1)} text="Bad" />
+
+            <h1>Statistics</h1>
+            Good: <Label text={good} /><br></br>
+            Neutral: <Label text={neutral} /><br></br>
+            Bad: <Label text={bad} /><br></br>
+        </div>
+    )
+}
+
+export default App
