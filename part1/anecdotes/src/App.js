f1022b3ccde53499271e5b0a7dd99bd5d205bf09
9600bauds <blasaladio3@gmail.com>
2023-05-23 20:31:15 -0300

    Exercise 1.14.

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -9,6 +9,8 @@ const Button = ({ clickAction, text }) => {
 }
 
 const App = () => {
+
+
     const anecdotes = [
         'If it hurts, do it more often.',
         'Adding manpower to a late software project makes it later!',
@@ -19,6 +21,13 @@ const App = () => {
         'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
         'The only way to go fast, is to go well.'
     ]
+
+    const increaseScore = (index) => {
+        let copy = [...scores] //clone the array
+        copy[index] += 1 //we can modify a const array because we are powerful
+        setScores(copy)
+    }
+
     const totalAnecdotes = anecdotes.length
     const getRandomAnecdoteIndex = () => {
         const index = Math.floor(Math.random() * totalAnecdotes)
@@ -27,27 +36,35 @@ const App = () => {
     const selectRandomAnecdote = () => {
         setSelected(getRandomAnecdoteIndex())
     }
+    const getHighestVotedAnecdoteIndex = () => {
+        const max = Math.max(...scores);
+        const index = scores.indexOf(max);
+        return index
+    }
 
     const initialSelectedState = getRandomAnecdoteIndex()
     const [selected, setSelected] = useState(initialSelectedState)
 
-    const increaseScore = (index) => {
-        const copy = { ...scores }
-        copy[index] += 1 //we can modify a const array because we are powerful
-        setScores(copy)
-    }
-
     const initialScoreState = Array(totalAnecdotes).fill(0)
     const [scores, setScores] = useState(initialScoreState)
 
+    const highestVotedAnecdoteIndex = getHighestVotedAnecdoteIndex() //i guess we can just define this once?    
+
+
     return (
         <div>
+            <h1>Anecdote of the Day</h1>
             {anecdotes[selected]}
             <br></br>
             Has {scores[selected]} votes
             <br></br>
             <Button clickAction={() => increaseScore(selected)} text="Vote" />
             <Button clickAction={() => selectRandomAnecdote()} text="Next Anecdote" />
+            <br></br>
+            <h1>Anecdote with the Most Votes</h1>
+            {anecdotes[highestVotedAnecdoteIndex]}
+            <br></br>
+            Has {scores[highestVotedAnecdoteIndex]} votes
         </div>
         
     )
