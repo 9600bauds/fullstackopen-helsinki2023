385de5cb8f652ec5fef6f2bda87a0c403146f060
9600bauds <blasaladio3@gmail.com>
2023-07-07 23:43:16 -0300

    Exercises 4.11-4.12.

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');const supertest = require('supertest');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
 const app = require('../app');
 const Blog = require('../models/blogSchema');
 const helper = require('./test_helper');
@@ -57,6 +58,21 @@ test('a valid blog can be added', async () => {
   expect(titles).toContain('I Was A Wizard The Whole Time');
 });
 
+test('blog without title cannot be added', async () => {
+  const newBlog = {
+    _id: '64a711a238976db70dedbeef',
+    url: 'www.youplume.com/r/watch?=thing',
+    author: 'imadb',
+    likes: 72,
+    __v: 0,
+  };
+
+  await api.post('/api/blogs').send(newBlog).expect(400);
+
+  const blogs = await helper.blogsInDb();
+  expect(blogs).toHaveLength(helper.initialBlogs.length);
+});
+
 test('blog without url cannot be added', async () => {
   const newBlog = {
     _id: '64a711a238976db70dedbeef',
@@ -70,6 +86,28 @@ test('blog without url cannot be added', async () => {
 
   const blogs = await helper.blogsInDb();
   expect(blogs).toHaveLength(helper.initialBlogs.length);
+});
+
+test('blog without likes becomes blog with 0 likes', async () => {
+  const newBlog = {
+    _id: '64a711a238976db70dedbeef',
+    title: 'I Was A Wizard The Whole Time',
+    author: 'imadb',
+    url: 'www.youplume.com/r/watch?=thing',
+    __v: 0,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const blogs = await helper.blogsInDb();
+  expect(blogs).toHaveLength(helper.initialBlogs.length + 1);
+
+  const blog = blogs.find((r) => r.title === 'I Was A Wizard The Whole Time');
+  expect(blog.likes).toBe(0);
 });
 
 test('unique identifier is named "id"', async () => {
