f3d8d3cff31d8726d9bb6f1146dfc1592f555212
9600bauds <blasaladio3@gmail.com>
2023-09-27 17:01:30 -0300

    Exercise 5.1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import blogService from './services/blogs';
+import loginService from './services/login';
+import Blog from './components/Blog';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [username, setUsername] = useState(''); 
+    const [password, setPassword] = useState(''); 
+    const [user, setUser] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        async function getData(){
+            const allBlogs = await blogService.getAll();
+            setBlogs(allBlogs);
+            console.log(allBlogs);
+        }
+        getData();
+    }, []);
+
+    const handleLogin = async (event) => {
+        event.preventDefault(); //do not refresh, do not pass go, do not collect 200$
+        if (!username || !password) {
+            toast.error('Username and password are required!');
+            return;
+        }
+        
+        try {
+            const user = await loginService.login({
+                username, password,
+            });
+            setUser(user);
+            setUsername('');
+            setPassword('');
+        } catch (exception) {
+            toast.error('Wrong credentials!');
+            return;
+        }
+    };
+
+    function getLoginOrBloglist(){
+        if (user === null) {
+            return (
+                <form onSubmit={handleLogin}>
+                    <div>
+                        username
+                        <input
+                            type="text"
+                            value={username}
+                            name="Username"
+                            onChange={({ target }) => setUsername(target.value)}
+                        />
+                    </div>
+                    <div>
+                        password
+                        <input
+                            type="password"
+                            value={password}
+                            name="Password"
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                    </div>
+                    <button type="submit">login</button>
+                </form>
+            );
+        }
+        
+        else {
+            return (
+                <div>
+                    <h2>Welcome, {user.name}.</h2>
+                    <h3>List of All Blogs</h3>
+                    {blogs.map(blog =>
+                        <Blog key={blog.id} blog={blog} />
+                    )}
+                </div>
+            );
+        }
+    }
+
+    return (
+        <>
+            <ToastContainer />
+            {getLoginOrBloglist()}
+        </>
+    );
 }
 
-export default App
+export default App;
