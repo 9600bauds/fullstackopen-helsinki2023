dbb0ece4db1936091dd597462467aa766862f605
9600bauds <blasaladio3@gmail.com>
2023-09-27 17:00:58 -0300

    Linter and packages

--- part5/bloglist-frontend/.eslintrc.cjs
+++ part5/bloglist-frontend/.eslintrc.cjs
@@ -8,15 +8,22 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: {"jsx": true} },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    indent: ['error', 4],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'off',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     "react/prop-types": 0, //we'll get back to this later
-    "no-unused-vars": warn, //we'll get back to this later
+    "no-unused-vars": 0, //we'll get back to this later
   },
 }
