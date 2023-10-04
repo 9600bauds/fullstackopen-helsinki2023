8642f6e2e1eb1c4ac589f3e368faa75cda4bafdd
9600bauds <blasaladio3@gmail.com>
2023-10-04 15:34:55 -0300

    Blog is toggleable

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,6 +1,13 @@
+import Togglable from './Togglable';
+
 const Blog = ({ blog }) => (
     <div>
-        {blog.title}, by {blog.author.name}
+        {blog.title}
+        <Togglable openLabel='view' closeLabel ='close'>
+            <div>{blog.url}</div>
+            <div>{blog.likes}<button>like</button></div>
+            <div>{blog.author.name}</div>
+        </Togglable>
     </div>  
 );
   
