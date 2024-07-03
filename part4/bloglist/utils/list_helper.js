96e0db137e0cae30ab0ed8b14719ee8ed3a5141c
9600bauds <blasaladio3@gmail.com>
2024-07-03 17:37:19 -0300

    Exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,18 @@
+
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const summer = (sum, blog) => { //this const named during winter
+    return sum + blog.likes
+  }
+
+  return blogs.reduce(summer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
