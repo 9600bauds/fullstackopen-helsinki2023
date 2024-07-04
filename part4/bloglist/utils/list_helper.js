5bcd9eebfa83a110797915cdfe8a36153672ca08
9600bauds <blasaladio3@gmail.com>
2024-07-04 19:53:25 -0300

    Exercise 4.5* with sexy reduce logic

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -12,7 +12,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce(summer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const maxxer = (max, blog) => { //chilling out maxxing relaxing all cool
+    if (!max || blog.likes > max.likes) {
+      return blog
+    }
+    return max
+  }
+  return blogs.reduce(maxxer, null)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
