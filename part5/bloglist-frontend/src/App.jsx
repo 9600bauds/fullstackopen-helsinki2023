3c7d6ed577795d31cf762deafd96873c9125b95a
9600bauds <blasaladio3@gmail.com>
2024-08-03 14:40:07 -0300

    Exercise 5.1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,15 +1,47 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState(``);
+  const [password, setPassword] = useState(``);
+  const [user, setUser] = useState(null);
+  
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    
+    const user = await loginService.login({
+      username, password,
+    });
+    setUser(user);
+    setUsername(``);
+    setPassword(``);
+  };
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then(blogs => {
+      setBlogs( blogs );
+    }
+    );  
+  }, []);
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to continue</h2>
+        <LoginForm
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+        />
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -18,7 +50,7 @@ const App = () => {
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
