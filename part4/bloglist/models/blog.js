c6ed02071d7f19d2f73a1f21d5a85c40f970a8e1
9600bauds <blasaladio3@gmail.com>
2024-08-04 17:23:46 -0300

    URL minlength is 3 so we have something to error

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: String,
-  url: { type: String, required: true },
+  url: { type: String, required: true, minLength: 3 },
   likes: { type: Number, default: 0 },
   user: {
     type: mongoose.Schema.Types.ObjectId,
