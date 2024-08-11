cee726444f6b5de83b02bdb561c3e1640a2bd6c3
9600bauds <blasaladio3@gmail.com>
2024-08-11 16:38:40 -0300

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: `solid`,
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
+
+  if(!expanded){
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={toggleExpanded}>view</button>
+      </div>  
+    );
+  }
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleExpanded}>hide</button>
+      <div>
+        {blog.url}
+      </div>
+      <div>
+        Likes: {blog.likes} <button>like!</button>
+      </div>
+      <div>
+        submitted by {blog.user.name}
+      </div>
+    </div>  
+  );
+};
 
 export default Blog;
