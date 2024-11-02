30545bd3228afb76a9800667cf983ecb6e4894bb
9600bauds <blasaladio3@gmail.com>
2024-11-02 15:14:20 -0300

    Testing controller

--- part4/bloglist/controllers/testing.js
+++ part4/bloglist/controllers/testing.js
@@ -1,13 +1,21 @@
 const testingRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const testHelper = require('../tests/test_helper')
 
 testingRouter.post('/reset', async (request, response) => {
-  console.log(`Resetting everything`);
   await User.deleteMany({})
   await Blog.deleteMany({})
 
   response.status(204).end()
 })
+
+// Endpoint used to reset the testing database to an initial testing state, with a few blogs and users already saved
+testingRouter.post('/seed', async (request, response) => {
+  await testHelper.setupTestDB(testHelper.initialUsers, testHelper.initialBlogs)
+
+  response.status(204).end()
+})
+
 
 module.exports = testingRouter
