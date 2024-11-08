37a68c2a37fa66468acbddc73ea186e42345ef4e
9600bauds <blasaladio3@gmail.com>
2024-11-08 19:12:53 -0300

    Exercises 6.1-6.2

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -1,23 +1,40 @@
+/* eslint-disable no-case-declarations */
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
-  }
-  
-}
+    case "GOOD":
+      const previousGoods = state.good;
+      const newGoods = previousGoods + 1;
 
-export default counterReducer
+      // Use the spread operator to create a new state with the "good" property overwritten
+      return {
+        ...state,
+        good: newGoods,
+      };
+    case "OK":
+      const newOks = state.ok + 1;
+
+      // Use the spread operator to create a new state with the "ok" property overwritten
+      return {
+        ...state,
+        ok: newOks,
+      };
+    case "BAD":
+      return {
+        ...state,
+        bad: state.bad + 1,
+      };
+    case "ZERO":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -1,35 +1,89 @@
-import deepFreeze from 'deep-freeze'
-import counterReducer from './reducer'
+import deepFreeze from "deep-freeze";
+import counterReducer from "./reducer";
 
-describe('unicafe reducer', () => {
+describe("unicafe reducer", () => {
   const initialState = {
     good: 0,
     ok: 0,
-    bad: 0
-  }
+    bad: 0,
+  };
 
-  test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
+  test("should return a proper initial state when called with undefined state", () => {
     const action = {
-      type: 'DO_NOTHING'
-    }
+      type: "DO_NOTHING",
+    };
 
-    const newState = counterReducer(undefined, action)
-    expect(newState).toEqual(initialState)
-  })
+    const newState = counterReducer(undefined, action);
+    expect(newState).toEqual(initialState);
+  });
 
-  test('good is incremented', () => {
+  test("should return the same state when called with an undefined action", () => {
     const action = {
-      type: 'GOOD'
-    }
-    const state = initialState
+      type: undefined,
+    };
+    const state = initialState;
 
-    deepFreeze(state)
-    const newState = counterReducer(state, action)
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual(initialState);
+  });
+
+  test("good is incremented", () => {
+    const action = {
+      type: "GOOD",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
     expect(newState).toEqual({
       good: 1,
       ok: 0,
-      bad: 0
-    })
-  })
-})
+      bad: 0,
+    });
+  });
+
+  test("ok is incremented", () => {
+    const action = {
+      type: "OK",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+    });
+  });
+
+  test("bad is incremented", () => {
+    const action = {
+      type: "BAD",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1,
+    });
+  });
+
+  test("zero returns 0 in all fields", () => {
+    const action = {
+      type: "ZERO",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0,
+    });
+  });
+});
