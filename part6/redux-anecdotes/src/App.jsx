4699047d89d7d5eda949f1c9cefc30d002d427ad
9600bauds <blasaladio3@gmail.com>
2024-11-10 23:05:43 -0300

    Exercise 6.14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
+
 import NewAnecdoteForm from "./components/NewAnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import FilterInput from "./components/FilterInput";
 import Notification from "./components/Notification";
 
+import anecdoteService from "./services/anecdotes";
+import { setAnecdotes } from "./reducers/anecdoteReducer";
+import { useDispatch } from "react-redux";
+
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService.getAll().then((notes) => dispatch(setAnecdotes(notes)));
+  }, []);
+
   return (
     <div>
       <FilterInput />
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -10,23 +10,19 @@ const contentToAnecdoteObject = (content) => {
   };
 };
 
-const initialContents = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-const initialState = initialContents.map(contentToAnecdoteObject);
-
 const anecdoteSlice = createSlice({
   name: "anecdotes",
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       const newAnecdote = contentToAnecdoteObject(action.payload);
       state.push(newAnecdote); //Will immer play nice and actually let me mutate the state this time?
+    },
+    appendAnecdotes(state, action) {
+      state.push(action.payload);
+    },
+    setAnecdotes(state, action) {
+      return action.payload;
     },
     vote(state, action) {
       const id = action.payload;
@@ -45,5 +41,6 @@ const anecdoteSlice = createSlice({
   },
 });
 
-export const { createAnecdote, vote } = anecdoteSlice.actions;
+export const { createAnecdote, appendAnecdotes, setAnecdotes, vote } =
+  anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
