657047a7d3afabd52724149c3828615f217e27a8
9600bauds <blasaladio3@gmail.com>
2024-11-25 23:35:24 -0300

    Exercise 7.4

--- part7/routed-anecdotes/src/components/NewAnecdoteForm.jsx
+++ part7/routed-anecdotes/src/components/NewAnecdoteForm.jsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
+import useField from "../hooks/useField";
 
-export const NewAnecdoteForm = (props) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
-
+export const NewAnecdoteForm = ({ addNew }) => {
+  const content = useField('content');
+  const author = useField('author');
+  const info = useField('info');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.addNew({
-      content,
-      author,
-      info,
+    addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     });
   };
@@ -22,15 +21,15 @@ export const NewAnecdoteForm = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
---
+++ part7/routed-anecdotes/src/hooks/useField.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+/**
+ * This is a custom hook for use in input fields.
+ * Since it has a type, a value, and an onChange, it can be easily used via spread operator as such:
+ * 
+ * const content = useField('content');
+ * <input {...content} />
+ */
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue()
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+    reset
+  }
+}
+
+export default useField
