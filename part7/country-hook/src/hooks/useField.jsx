bced89c9f41db425f5ed291be369de0448530660
9600bauds <blasaladio3@gmail.com>
2024-11-26 21:06:06 -0300

    Refactors into individual files

---
+++ part7/country-hook/src/hooks/useField.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+/**
+ * This is a custom hook for use in input fields.
+ * Since it has a type, a value, and an onChange, it can be easily used via spread operator as such:
+ * 
+ * const content = useField('content');
+ * <input {content.toInput()} />
+ */
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  const toInput = () => {
+    return {
+      type,
+      value,
+      onChange
+    }
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+    reset,
+    toInput
+  }
+}
+
+export default useField
