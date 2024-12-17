177c0f729ae1c5858611a7bd40c8146c29c3813c
9600bauds <blasaladio3@gmail.com>
2024-12-17 18:31:41 -0300

    Exercise 8.6

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -115,6 +116,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -132,6 +142,28 @@ const resolvers = {
       return matchingBooks
     },
     allAuthors: () => authors,
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+
+      let authorFound = false;
+      for (let author of authors) {
+        if (author.name === book.author) {
+          authorFound = true;
+          break;
+        }
+      }
+      if (!authorFound) {
+        //Ideally we could have an addAuthor mutation and call that... right?
+        const newAuthor = { name: book.author, id: uuid() }
+        authors = authors.concat(newAuthor)
+      }
+
+      return book
+    },
   },
 
   Author: {
