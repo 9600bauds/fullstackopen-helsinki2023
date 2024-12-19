3c907d86cb92720c35fdc79dac8dd86b59db7246
9600bauds <blasaladio3@gmail.com>
2024-12-18 21:31:53 -0300

    Exercise 8.12 with React Select

--- part8/library-frontend/src/components/EditAuthorBornForm.jsx
+++ part8/library-frontend/src/components/EditAuthorBornForm.jsx
@@ -1,12 +1,14 @@
-import { useMutation } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { EDIT_AUTHOR_BORN, ALL_AUTHORS } from '../queries';
 import useField from '../hooks/useField';
 import { useNotifications } from '../hooks/useNotifications';
+import { useState } from 'react';
+import Select from 'react-select';
+import { Spinner } from 'react-bootstrap';
 
 const EditAuthorBornForm = () => {
   const { successMessage, errorMessage } = useNotifications();
-
-  const name = useField(`name`);
+  const [name, setName] = useState(``);
   const born = useField(`born`);
 
   const [editAuthorBorn] = useMutation(EDIT_AUTHOR_BORN, {
@@ -23,21 +25,41 @@ const EditAuthorBornForm = () => {
     },
   });
 
+  const authorNameQuery = useQuery(ALL_AUTHORS);
+
+  if (authorNameQuery.loading) {
+    return (
+      <div className="d-flex justify-content-center mt-3">
+        <Spinner animation="border" />
+      </div>
+    );
+  }
+
+  const authorNames = authorNameQuery.data.allAuthors;
+
+  const selectOptions = authorNames.map((author) => {
+    return { value: author.name, label: author.name };
+  });
+  const optionSelected = (option) => {
+    setName(option.value);
+  };
+
   const submit = async (event) => {
     event.preventDefault();
 
     await editAuthorBorn({
-      variables: { name: name.value, setBornTo: parseInt(born.value) },
+      variables: { name: name, setBornTo: parseInt(born.value) },
     });
   };
 
   return (
     <div>
       <form onSubmit={submit}>
-        <div>
-          name
-          <input {...name.toInput()} />
-        </div>
+        <Select
+          defaultValue={name}
+          onChange={optionSelected}
+          options={selectOptions}
+        />
         <div>
           birthyear
           <input {...born.toInput()} />
