710b274f7fcdc666659a4326a00802ff147e1c03
9600bauds <blasaladio3@gmail.com>
2024-12-19 19:32:25 -0300

    Mongoose refactoring

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,7 +1,31 @@
-const { ApolloServer } = require(`@apollo/server`);
-const { startStandaloneServer } = require(`@apollo/server/standalone`);
-const { GraphQLError } = require(`graphql`);
-const { v1: uuid } = require(`uuid`);
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { GraphQLError } from 'graphql';
+import { v1 as uuid } from 'uuid';
+
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import typeDefs from './typeDefs.js';
+
+import Book from './models/Book.js';
+import Author from './models/Author.js';
+
+mongoose.set(`strictQuery`, false);
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+console.log(`connecting to`, MONGODB_URI);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log(`connected to MongoDB`);
+  })
+  .catch((error) => {
+    console.log(`error connection to MongoDB:`, error.message);
+  });
 
 let authors = [
   {
@@ -28,20 +52,6 @@ let authors = [
     id: `afa5b6f3-344d-11e9-a414-719c6709cf3e`,
   },
 ];
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
- */
 
 let books = [
   {
@@ -94,44 +104,6 @@ let books = [
     genres: [`classic`, `revolution`],
   },
 ];
-
-const typeDefs = `
-  type Book {
-    title: String!
-    published: Int
-    author: String!
-    genres: [String!]!
-    id: ID!
-  }
-
-  type Author {
-    name: String!
-    born: Int
-    id: ID!
-    bookCount: Int!
-  }
-
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks(author: String, genre: String): [Book!]!
-    allAuthors: [Author!]!
-  }
-
-  type Mutation {
-    addBook(
-      title: String!
-      author: String!
-      published: Int
-      genres: [String!]!
-    ): Book
-
-    editAuthor(
-      name: String!
-      setBornTo: Int
-    ) : Author
-  }
-`;
 
 const resolvers = {
   Query: {
@@ -224,7 +196,7 @@ const server = new ApolloServer({
 });
 
 startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: process.env.PORT || 4000 },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
