e2a9c50d0ab9077ee8aa7ca5e360412164dddbd5
9600bauds <blasaladio3@gmail.com>
2025-09-26 23:18:47 -0300

    Exercise 9.17 + prettier

---
+++ part9/flight-diary-frontend/src/DiaryEntryComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import type { DiaryEntry } from './types';
+
+const DiaryEntryComponent: React.FC<{ entry: DiaryEntry }> = ({ entry }) => {
+  return (
+    <div>
+      <h3>{entry.date}</h3>
+      <p>Weather: {entry.weather}</p>
+      <p>Visibility: {entry.visibility}</p>
+      <p>Comment: {entry.comment}</p>
+    </div>
+  );
+};
+
+export default DiaryEntryComponent;
